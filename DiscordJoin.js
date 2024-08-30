d048bb982f6297de21e1f69915dd3f1cb1062894
MiaaHarper <miaharper.contact@gmail.com>
2024-08-30 23:18:38 +0200

    Add files via upload

---
+++ DiscordJoin.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.once('ready', () => {
+    console.log('Le bot est prêt.');
+    client.guilds.cache.forEach(guild => {
+        console.log(`Bot connecté sur le serveur : ${guild.name} (ID: ${guild.id})`);
+        console.log(`Nombre de membres : ${guild.memberCount}`);
+    });
+});
+
+client.on('guildMemberAdd', member => {
+    console.log(member.guild.memberCount);
+});
+
+client.on('guildMemberRemove', member => {
+    console.log(member.guild.memberCount);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+
